ece27b3b2668f8ecc02384711c0b3e0d24dd990d
tien998 <tienvn998@gmail.com>
2025-08-14 20:16:32 +0700

    fix autoPlay in mobile

--- src/app/comps/Banner.tsx
+++ src/app/comps/Banner.tsx
@@ -6,7 +6,7 @@ export default function Banner() {
 
     return <section className="relative -z-10 h-[500px] md:h-[850px]">
         <Video
-            sources={bannerSources} autoPlay loop muted
+            sources={bannerSources} autoPlay loop muted playsInline
             className="h-full md:w-full object-cover"
         />
         <div className="z-10 absolute bottom-0 text-white">
