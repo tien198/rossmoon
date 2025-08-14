2c767d1bf29a9c3870ed5d164468ea138393a31c
tien998 <tienvn998@gmai.com>
2025-08-14 11:55:55 +0700

    fix image display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import banner from '@/assets/river-forest-sunset-landscape-art.jpg'
+// import banner from '@/assets/river-forest-sunset-landscape-art.jpg'
 
 export default function Home() {
   return (
     <main >
-      <div className="relative" style={{height:900}}>
+      <div className="relative h-56 md:h-[900px]" >
 
-        <Image src={banner.src} alt=""
+        <Image src={'/river-forest-sunset-landscape-art.jpg'} alt=""
           fill
         // width={1080} height={720}
         />
