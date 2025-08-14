1ea33d178fac30a23fb31bc03743630a57108e9e
tien998 <tienvn998@gmail.com>
2025-08-14 21:07:55 +0700

    complete banner

--- src/app/comps/Banner.tsx
+++ src/app/comps/Banner.tsx
@@ -1,16 +1,21 @@
 import Video from "@/components/Video";
+import Link from "next/link";
 
 
 export default function Banner() {
     const bannerSources = useGetBannerSources()
+    const story = useGetStory()
 
     return <section className="relative -z-10 h-[500px] md:h-[850px]">
         <Video
             sources={bannerSources} autoPlay loop muted playsInline
             className="h-full md:w-full object-cover"
         />
-        <div className="z-10 absolute bottom-0 text-white">
-            <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt perferendis impedit harum explicabo, sint excepturi odit, magni repudiandae quasi aut ad quod temporibus accusantium hic amet laborum maxime nesciunt doloremque!</div>
+        <div className="z-10 absolute -translate-1/2 left-1/2 bottom-0 text-white
+            flex flex-col justify-between items-center gap-5 text-center">
+                <div className="font-extralight text-sm md:text-base">{story.category}</div>
+                <div className="text-2xl md:text-5xl">{story.storyName}</div>
+                <Link href='/' className='border-b border-white'>Đăng ký nhận thông báo</Link>
         </div>
     </section>
 }
@@ -37,4 +42,12 @@ function useGetBannerSources() {
         }
     ]
     return bannerSources
+}
+
+
+function useGetStory() {
+    return {
+        category: 'Làm đẹp',
+        storyName: ' La Beauté Louis Vuitton'
+    }
 }
