0ed95f6e6d2325b3b5b8eebb1f119430f76fe1f2
tien998 <tienvn998@gmail.com>
2025-08-23 23:19:19 +0700

    fix footer

---
+++ src/components/layout/hooks/useGetMenuItem.ts
@@ -0,0 +1,118 @@
+// hierachical struture with
+
+import { MenuItem } from "@/types/menuItem";
+
+//  3 level
+export function useGetMenuItems(): MenuItem {
+    return {
+        display: 'cate',
+        nameDisplay: '',
+        categoryItems: [
+            {
+                display: 'cate',
+                nameDisplay: "Quà tặng và dịch vụ cá nhân hóa",
+                // 2
+                categoryItems: [
+                    {
+                        display: 'link',
+                        nameDisplay: "Tất cả quà tặng",
+                        url: 'none'
+                    },
+                    {
+                        display: 'cate',
+                        nameDisplay: "Quà tặng cho nữ",
+                        // 3
+                        categoryItems: [
+                            {
+                                display: 'link',
+                                nameDisplay: "Sản phẩm mới",
+                                url: 'none',
+                            },
+                        ]
+                    },
+                    {
+                        display: 'cate',
+                        nameDisplay: "Quà tặng cho nữ",
+                        // 3
+                        categoryItems: [
+                            {
+                                display: 'link',
+                                nameDisplay: "Túi xách và ví",
+                                url: 'none',
+                            },
+                            {
+                                display: 'link',
+                                nameDisplay: "Đồ Nữ",
+                                url: 'none',
+                            },
+                        ]
+                    },
+                ]
+            },
+            {
+                display: 'cate',
+                nameDisplay: "Sản phẩm mới",
+                // 3
+                categoryItems: [
+                    {
+                        display: 'link',
+                        nameDisplay: "Đồ Nữ",
+                        url: 'none',
+                    },
+                    {
+                        display: 'link',
+                        nameDisplay: "Đồ Nam",
+                        url: 'none',
+                    },
+                ]
+            },
+
+            {
+                display: 'link',
+                nameDisplay: "Túi xách và ví",
+                url: 'none',
+            },
+            {
+                display: 'link',
+                nameDisplay: "Đồ Nữ",
+                url: 'none',
+            },
+            {
+                display: 'link',
+                nameDisplay: "Đồ Nam",
+                url: 'none',
+            },
+            {
+                display: 'link',
+                nameDisplay: "Trang sức",
+                url: 'none',
+            },
+            {
+                display: 'link',
+                nameDisplay: "Đồng hồ",
+                url: 'none',
+            },
+            {
+                display: 'link',
+                nameDisplay: "Nước hoa",
+                url: 'none',
+            },
+            {
+                display: 'link',
+                nameDisplay: "Rương, Phụ kiện du lịch và Trang trí nội thất",
+                url: 'none',
+            },
+            {
+                display: 'link',
+                nameDisplay: "Dịch vụ",
+                url: 'none',
+            },
+            {
+                display: 'link',
+                nameDisplay: "Maison Louis Vuitton",
+                url: 'none',
+            },
+        ]
+    }
+}
+
