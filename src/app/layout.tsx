0363c99c7ceb8ad9b95c3672fb9a46bc3a46e9f6
tien998 <tienvn998@gmail.com>
2025-08-26 11:39:16 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 import Header from "@/components/layout/Header/Header";
 import Footer from "@/components/layout/Footer";
 
@@ -28,7 +28,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
-        {children}
+        <main>
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,10 @@ import Banner from "./comps/Banner";
 
 export default function Home() {
   return (
-    <main >
+    <>
       {/* Search bar */}
-      <div className="h-18"></div>
       <SearchBar />
       <Banner />
-    </main>
+    </>
   );
 }
