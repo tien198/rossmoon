e1cd0243354c62604a7b34bf34e9e5aff9a5296e
tien998 <tienvn998@gmail.com>
2025-09-03 20:27:20 +0700

    fix type error to deploy

--- src/app/products/route.ts
+++ src/app/products/route.ts
@@ -6,17 +6,17 @@ export async function GET(req: NextRequest) {
     return NextResponse.json(prods)
 }
 
-export async function POST(req: NextRequest) {
-    const created = await ProductImp.create({
-        name: 'prod', imageUrl: 'none', price: 12, priceFormatted: '12',
-        description: "",
-        width: 0,
-        height: 0,
-        depth: 0,
-        features: []
-    })
-    return NextResponse.json(created)
-}
+// export async function POST(req: NextRequest) {
+//     const created = await ProductImp.create({
+//         name: 'prod', imageUrl: 'none', price: 12, priceFormatted: '12',
+//         description: "",
+//         width: 0,
+//         height: 0,
+//         depth: 0,
+//         features: []
+//     })
+//     return NextResponse.json(created)
+// }
 
 export async function DELETE(req: NextRequest) {
     const deleteResult = await ProductImp.deleteById('68b069e88872871c1605f5c5')
