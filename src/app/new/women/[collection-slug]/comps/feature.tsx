296991f1054f8bc3032892e29e2a96473a8c9c44
tien998 <tienvn998@gmai.com>
2025-09-17 20:08:48 +0700

    update

--- src/app/new/women/[collection-slug]/comps/feature.tsx
+++ src/app/new/women/[collection-slug]/comps/feature.tsx
@@ -21,12 +21,12 @@ export default function Feature(fea: MagazineFeaturePart) {
 
 function ProductCard(prod: ProductPart) {
     return (
-        <div className={"relative aspect-[3/4] "+ styles['card']}>
+        <div className={"relative w-full aspect-[2.5/4] md:aspect-[3/4] "+ styles['card']}>
             <div className={styles['card__container-border']}></div>
             <Image src={prod.attributes?.medias[0]?.url ?? ''} alt={prod.name!} width={300} height={300} />
-            <span className="absolute left-3 bottom-0 md:left-15 md:bottom-15 flex flex-col ">
-                <span>{prod.name}</span>
-                <span>{prod.price}</span>
+            <span className="absolute left-3 bottom-4 md:left-15 md:bottom-15 flex flex-col gap-3">
+                <span className={styles['shadow']}>{prod.name}</span>
+                <span>{prod.price?.toLocaleString()} <sup>đ</sup></span>
             </span>
             {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia rem cumque debitis sed repellendus molestias laudantium vitae atque odit impedit beatae, enim perferendis similique qui tempore fugit! Aut, rem quos? */}
         </div>
