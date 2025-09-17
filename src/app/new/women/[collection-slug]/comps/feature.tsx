46433e495fb0ebdf6fff4ef7efc8e920207d5c34
tien998 <tienvn998@gmai.com>
2025-09-17 16:23:42 +0700

    update

---
+++ src/app/new/women/[collection-slug]/comps/feature.tsx
@@ -0,0 +1,34 @@
+import { MagazineFeaturePart } from "@/models/magazineFeature.zod";
+import { ProductPart } from "@/models/product.zod";
+import Image from "next/image";
+
+import styles from "./productCard.module.scss"
+
+export default function Feature(fea: MagazineFeaturePart) {
+    return (
+        <>
+            {
+                fea.bannerImage
+                && <div className="col-span-2 row-span-2 overflow-hidden aspect-[3/4]"><Image src={fea.bannerImage.desktopUrl!} alt={(fea.title ?? fea.products?.[0]?.name) || 'alt'} height={300} width={300} className="h-full w-full object-cover" /></div>
+            }
+            {fea.products?.map(prod =>
+                <ProductCard {...prod} key={prod._id?.toString()} />
+            )}
+        </>
+    )
+}
+
+
+function ProductCard(prod: ProductPart) {
+    return (
+        <div className={"relative aspect-[3/4] "+ styles['card']}>
+            <div className={styles['card__container-border']}></div>
+            <Image src={prod.attributes?.medias[0]?.url ?? ''} alt={prod.name!} width={300} height={300} />
+            <span className="absolute left-3 bottom-0 md:left-15 md:bottom-15 flex flex-col ">
+                <span>{prod.name}</span>
+                <span>{prod.price}</span>
+            </span>
+            {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia rem cumque debitis sed repellendus molestias laudantium vitae atque odit impedit beatae, enim perferendis similique qui tempore fugit! Aut, rem quos? */}
+        </div>
+    )
+}
--- src/app/new/women/[collection-slug]/page.tsx
+++ src/app/new/women/[collection-slug]/page.tsx
@@ -1,5 +1,5 @@
 import MagazineFeatureImp from "@/models/magazineFeature"
-import Image from "next/image"
+import Feature from "./comps/feature"
 
 type Props = {
     params: {
@@ -13,20 +13,9 @@ export default async function New({ params }: Props) {
     const features = await MagazineFeatureImp.findByCollectionSlug(collectionSlug)
 
     return (
-        <div >
+        <div className="grid grid:col-2 md:grid-cols-4">
             {features.map(fea =>
-                <div key={fea._id?.toString()} className="grid grid-cols-4">
-                    {fea.bannerImage && <div className="col-span-2 row-span-2"><Image src={fea.bannerImage.desktopUrl!} alt={(fea.title ?? fea.products?.[0]?.name) || 'alt'} height={300} width={300} /></div>}
-                    {fea.products?.map((prod, idx) =>
-                        <>
-                            {idx === 0 && <div key={prod._id?.toString()} className="bg-amber-500">{prod.name}</div>}
-                            {idx === 1 && <div key={prod._id?.toString()} className="bg-amber-400">{prod.name}</div>}
-                            {idx === 2 && <div key={prod._id?.toString()} className="bg-amber-300">{prod.name}</div>}
-                            {idx === 3 && <div key={prod._id?.toString()} className="bg-amber-200">{prod.name}</div>}
-                            {idx > 3 && <div key={prod._id?.toString()} className="h-20 bg-amber-200">{prod.name}</div>}
-                        </>
-                    )}
-                </div>
+                <Feature {...fea} key={fea._id?.toString()} />
             )}
         </div>
     )
